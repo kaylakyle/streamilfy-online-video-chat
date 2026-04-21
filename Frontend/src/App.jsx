b17bdc683e59b21716aa0d038c7859c25aa7980c
lynnkati <131492945+kaylakyle@users.noreply.github.com>
2026-04-21 22:03:04 +0300

    pageloader,app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Route ,Routes} from 'react-router'
+import { Routes, Route, Navigate } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
 
 import HomePage from './pages/HomePage.jsx'
@@ -18,27 +18,30 @@ const App = () => {
   queryKey: ["authUser"],
 
     queryFn: async () => {
-      const res = awaitInstance.get("auth/me");
+      const res = awaitInstance.get("/auth/me");
       return res.data;
     },
+    retry:false, //auth check
   });
 
+
   const authUser = authData?.user
+
+
   return (
     <div className="h-screen"  data-theme="night">
    {/* <button onClick={() => toast.success("hello world")}>
         create a toast
       </button> */}
-      <Routes>  
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/notifications" element={<NotificationsPage />} />
-        <Route path="/call" element={<CallPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/onboarding" element={<OnboardingPage />} />
-
-      </Routes>
+      <Routes>
+  <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+  <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+  <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+  <Route path="/notifications" element={authUser ? <NotificationsPage /> : <Navigate to="/login" />} />
+  <Route path="/call" element={authUser ? <CallPage /> : <Navigate to="/login" />} />
+  <Route path="/chat" element={authUser ? <ChatPage /> : <Navigate to="/login" />} />
+  <Route path="/onboarding" element={authUser ? <OnboardingPage /> : <Navigate to="/login" />} />
+</Routes>
 
       <Toaster/>
  
