58ea57505d3500aded2b07d168695d19cf83acec
lynnkati <131492945+kaylakyle@users.noreply.github.com>
2026-04-25 22:25:50 +0300

    .

--- Frontend/src/pages/OnboardingPage.jsx
+++ Frontend/src/pages/OnboardingPage.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import useAuthUser from "../hooks/useAuthUser";
+// import useAuthUser from "../hooks/useAuthUser";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
-import { completeOnboarding } from "../lib/api";
+// import { completeOnboarding } from "../lib/api";
 import { LoaderIcon, MapPinIcon, ShipWheelIcon, ShuffleIcon } from "lucide-react";
-import { LANGUAGES } from "../constants";
+// import { LANGUAGES } from "../constants";
 
 const OnboardingPage = () => {
   const { authUser } = useAuthUser();
