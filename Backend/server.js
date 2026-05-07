a35ed288722f43b91e6286378e7df60ca158f7ee
lynnkati <131492945+kaylakyle@users.noreply.github.com>
2026-05-07 22:29:18 +0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,30 +1,35 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./lib/db.js";
-import authRoutes from "./routes/auth.route.js"
-import userRoutes from "./routes/user.route.js"
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
-import cookieParse from "cookie-parser";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
 dotenv.config();
-//start the server and connect to the database.
+
 connectDB();
-//start exress server.
+
 const app = express();
-const PORT = process.env.PORT;
 
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true // allow frontend to connect send with cookies.
+  origin: [
+    "http://localhost:5173",
+    "https://your-frontend.vercel.app"
+  ],
+  credentials: true
 }));
-app.use(express.json());
-app.use(cookieParse());
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Backend running");
 });
-//routes.
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+export default app;
