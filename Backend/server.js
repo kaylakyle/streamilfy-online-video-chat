ec936d36d913fbc61393b0b866e707ef8485e861
lynnkati <131492945+kaylakyle@users.noreply.github.com>
2026-05-24 23:03:39 +0300

    frontend and backend deployement setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,44 +1,128 @@
+// import express from "express";
+// import dotenv from "dotenv";
+// import connectDB from "./lib/db.js";
+// import authRoutes from "./routes/auth.route.js";
+// import userRoutes from "./routes/user.route.js";
+// import chatRoutes from "./routes/chat.route.js";
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+
+// dotenv.config();
+
+// connectDB();
+
+// const app = express();
+
+// //start server
+// const PORT = process.env.PORT || 5000;
+
+// app.use(cors({
+//   origin: [
+//     "http://localhost:5173",
+//     // "https://your-frontend.vercel.app"
+//   ],
+//   credentials: true
+// }));
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.get("/", (req, res) => {
+//   res.send("Backend running");
+// });
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/chat", chatRoutes);
+
+// export default app;
+
+// import express from "express";
+// import "dotenv/config";
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+// import path from "path";
+
+// import authRoutes from "./routes/auth.route.js";
+// import userRoutes from "./routes/user.route.js";
+// import chatRoutes from "./routes/chat.route.js";
+
+// import  connectDB  from "./lib/db.js";
+
+// const app = express();
+// const PORT = process.env.PORT;
+
+// const __dirname = path.resolve();
+
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true, // allow frontend to send cookies
+//   })
+// );
+
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/chat", chatRoutes);
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+//   app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+// });;
+// }
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+//   connectDB();
+// });
+
 import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./lib/db.js";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-
-dotenv.config();
-
-connectDB();
+import connectDB from "./lib/db.js";
 
 const app = express();
-
-//start server
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    // "https://your-frontend.vercel.app"
-  ],
-  credentials: true
+  origin: "http://localhost:5173",
+  credentials: true,
 }));
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
 
 app.use(express.json());
 app.use(cookieParser());
-
-app.get("/", (req, res) => {
-  res.send("Backend running");
-});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-export default app;
+if (process.env.NODE_ENV === "production") {
+  const distPath = path.join(__dirname, "../frontend/dist");
 
+  app.use(express.static(distPath));
 
+  app.use((req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
